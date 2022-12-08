58790bd263865d5f5a2f02a532cd8e43a037143f
1056432 <117742531+1056432@users.noreply.github.com>
2022-12-08 16:09:11 +0000

    Update libraries.r

--- functions/libraries.r
+++ functions/libraries.r
@@ -1,4 +1,4 @@
-#Purpose of script: contains packages needed in the penguin_assignment.rmd
+#Purpose of script: contains packages needed in the penguin_assignment_q4.rmd
 
 #The following packages can be loaded using this file
 library(palmerpenguins)
